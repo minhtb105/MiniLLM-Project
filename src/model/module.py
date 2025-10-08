3ada6d1d615030b1abcde2572041a9c4923ac26d
minhtb105 <tranbinhminh_t64@hus.edu.vn>
2025-10-08 11:43:01 +0700

    feat(core): implement base Module class with parameter and state management

---
+++ src/model/module.py
@@ -0,0 +1,81 @@
+import logging
+import numpy as np
+from tensor import Tensor
+
+
+class Module:
+    """
+    A base class similar to torch.nn.Module
+    Supports:
+        - Registering parameters
+        - Saving and loading weights
+        - Recursive access to submodles
+    """
+    def __init__(self):
+        self._modules = {}
+        self._parameters = {}
+
+    def add_module(self, name, module):
+        self._modules[name] = module
+
+    def add_parameter(self, name, tensor):
+        assert isinstance(tensor, Tensor)
+        self._parameters[name] = tensor
+        
+    def parameters(self):
+        """
+        Iterate over all parameters (including those of submodules).
+        Returns a list of Tensors with requires_grad=True.
+        """
+        params = []
+        for name, p in self._parameters.items():
+            if p.requires_grad:
+                params.append(p)
+            
+        for m in self._modules.values():
+            params.extend(m.parameters())
+            
+        return params
+
+    def named_parameters(self, prefix=""):
+        """
+        Iterate over all parameters with their names (for saving state_dict).
+        """
+        for name, p in self._parameters.items():
+            yield f"{prefix}{name}", p
+            
+        for mname, m in self._modules.items():
+            for sub_name, p in m.named_parameters(f"{prefix}{mname}."):
+                yield sub_name, p
+
+    def state_dict(self):
+        """
+        Return a dict mapping parameter names to NumPy arrays (tensor data).
+        """
+        return {name: p.data.copy() for name, p in self.named_parameters()}
+
+    def load_state_dict(self, state_dict):
+        """
+        Load weights from dict (name -> ndarray)
+        """
+        for name, p in self.named_parameters():
+            if name in state_dict:
+                p.data = np.array(state_dict[name], dtype=np.float32)
+            else:
+                print(f"[Warning] Missing key in state_dict: {name}")
+
+    def save_state_dict(self, path):
+        """
+        Save weights to file .npz
+        """
+        np.savez(path, **self.state_dict())
+        logging(f"Saved model weights to {path}")
+
+    def from_pretrained(self, path):
+        """
+        Load weights file .npz
+        """
+        state = np.load(path)
+        self.load_state_dict(state)
+        logging.info(f"Loaded model weights from {path}")
+        
